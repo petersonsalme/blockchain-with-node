f67927c876394332db5dd1e72a38d89b4804f139
Peterson Salme <petersonsalme@gmail.com>
2021-10-26 23:23:27 -0300

    Fetching wallet info from backend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
 
 class App extends Component {
+    state = { 
+        walletInfo: {
+            address: 'foo-xv6',
+            balance: 9999
+        } 
+    };
+    
+    componentDidMount() {
+        const url = 'http://localhost:3000/api/wallet-info';
+        fetch(url)
+            .then(response => response.json())
+            .then(json => this.setState({ walletInfo: json }));
+    }
+
     render() {
+        const { address, balance } = this.state.walletInfo;
+
         return(
             <div>
-                Welcome to the blockchain...
+                <div>Welcome to the blockchain...</div>
+                <div>Address: {address}</div>
+                <div>Balance: {balance}</div>
             </div>
         );
     }
