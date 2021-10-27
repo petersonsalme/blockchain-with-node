ad5c7c5f701ead4d9c4edb5d580b4eafcf5dd4f5
Peterson Salme <petersonsalme@gmail.com>
2021-10-26 23:02:17 -0300

    creating App component

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/App';
 
 render(
-    <div>Testing this feature</div>, 
+    <App />, 
     document.getElementById('root')
 );
