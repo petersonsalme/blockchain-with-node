8cf5c44f6047351cd5e4f34bce1b16fa64ab46b1
Peterson Salme <petersonsalme@gmail.com>
2021-10-27 23:17:26 -0300

    extracting block data to specific component

---
+++ client/src/components/Block.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Block extends Component {
+    render() {
+        const { timestamp, hash, data} = this.props.block;
+        
+        const hashDisplay = `${hash.substring(0, 15)}...`;
+        const stringfiedData = JSON.stringify(data);
+        const dataDisplay = stringfiedData.length > 35 ?
+            `${stringfiedData.substring(0, 35)}...` :
+            stringfiedData;
+
+        return (
+            <div className='Block'>
+                <div>Hash: {hashDisplay}</div>
+                <div>Timestamp: {new Date(timestamp).toLocaleDateString()}</div>
+                <div>Data: {dataDisplay}</div>
+            </div>
+        )
+    }
+}
+
+export default Block;
