44a39559e6936012146f8a74609e4b377fbc0466
Peterson Salme <petersonsalme@gmail.com>
2021-11-09 22:39:00 -0300

    Creates transaction component to display recipients correctly

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -1,20 +1,54 @@
 import React, { Component } from "react";
+import { Button } from 'react-bootstrap';
+import Transaction from './Transaction';
 
 class Block extends Component {
+    state = { displayTransaction: false };
+
+    toogleTransaction = () => {
+        this.setState({ displayTransaction: !this.state.displayTransaction })
+    }
+
+    get displayTransaction() {
+        const { data } = this.props.block;
+        const stringfiedData = JSON.stringify(data);
+        const dataDisplay = stringfiedData.length > 35 ? `${stringfiedData.substring(0, 35)}...` : stringfiedData;
+
+        if (this.state.displayTransaction) {
+            return (
+                <div>
+                    {
+                        data.map(t => (
+                            <div key={t.id}>
+                                <hr />
+                                <Transaction transaction={t}></Transaction>
+                            </div>
+                        ))
+                    }
+                    <br />
+                    <Button bsStyle="danger" bsSize="small" onClick={this.toogleTransaction}>Show Less</Button>
+                </div>
+            );
+        }
+
+        return (
+            <div>
+                <div>Display Transaction: {dataDisplay}</div>
+                <Button bsStyle="danger" bsSize="small" onClick={this.toogleTransaction}>Show More</Button>
+            </div>
+        );
+    }
+
     render() {
-        const { timestamp, hash, data} = this.props.block;
+        const { timestamp, hash} = this.props.block;
         
         const hashDisplay = `${hash.substring(0, 15)}...`;
-        const stringfiedData = JSON.stringify(data);
-        const dataDisplay = stringfiedData.length > 35 ?
-            `${stringfiedData.substring(0, 35)}...` :
-            stringfiedData;
-
+        
         return (
             <div className='Block'>
                 <div>Hash: {hashDisplay}</div>
                 <div>Timestamp: {new Date(timestamp).toLocaleDateString()}</div>
-                <div>Data: {dataDisplay}</div>
+                {this.displayTransaction}
             </div>
         )
     }
